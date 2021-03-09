bf85a8c1015aee729bec96c5ad94a5950f65ca64
Elena Surilova <elen21surilova@gmail.com>
2021-03-10 00:29:59 +0200

    add solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	if (n < 0) {
+		n = n * -1;
+	}
+	const str = String(n);
+	let arr = [];
+	for (let i = str.length - 1; i >= 0; i--) {
+		arr.push(str[i]);
+	}
+	const newStr = arr.join('');
+	return Number(newStr);
 }
